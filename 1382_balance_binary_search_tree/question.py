3a816748c89fa11123083d0efa956745c7cebddb
Flavio Vieira Leao <flavio.vl@gmail.com>
2025-06-27 22:25:06 -0300

    rename dirs

--- 1382_balance_binary_search_tree/question.py
+++ 1382_balance_binary_search_tree/question.py
@@ -1,11 +1,11 @@
-class TreeNode(object):
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-class Solution(object):
+class Solution:
     def balanceBST(self, root):
         def inorder(node):
             if not node:
